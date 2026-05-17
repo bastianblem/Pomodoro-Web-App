a58d5b8ec6e913aadc891beceb23a89819b0fdac
bastianblem <bastian.buettner@student.uni-luebeck.de>
2026-05-17 02:22:15 +0200

    Added basic structure; Added dynamic Button states/styles via CTAButton.vue

---
+++ app/components/CTAButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button :class="classes">
+        {{ value }}
+    </button>
+</template>
+<script setup lang="ts">
+import {computed} from 'vue';
+// TODO: I want to implement a dynamic cta button logic that changes visually between states of buttons
+type ButtonType = "Start" | "Pause" | "Reset"
+
+const props = defineProps<{
+    value: ButtonType
+}>()
+
+const styles = {
+    Start: {
+        base: 'bg-indigo-500 text-white hover:bg-indigo-600',
+    },
+    Pause: {
+        base: 'bg-amber-500 text-white hover:bg-amber-600',
+    },
+    Reset: {
+        base: 'bg-red-500 text-white hover:bg-red-600'
+    }
+} as const
+
+const classes = computed(() => {
+    return `
+        px-4 px-2 rounded-xl font-medium transition ${styles[props.value].base}
+    `
+})
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,9 @@
 <template>
     <h1>Hello World!</h1>
+    <CTAButton value="Start"/>
+    <CTAButton value="Pause"/>
+    <CTAButton value="Reset"/>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CTAButton from '@/components/CTAButton.vue'
+</script>
